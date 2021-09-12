2d472e8d2292adc97a6d14b613b8465af7427aa3
Jay Peper <jason.peper@gamesys.cco.uk>
2021-09-12 12:12:17 +0100

    Add party names, fix some bugs

--- src/App.js
+++ src/App.js
@@ -23,21 +23,21 @@ const App = () => {
   const classes = useStyles();
   const [loaded, setLoaded] = useState(false);
   const [source, setSource] = useState('');
-  const [data2, setData2] = useState({});
+  const [partyResults, setPartyResults] = useState({});
   const seats = 58
   const parties = {
-    D1: 'party 1',
-    D2: 'my party',
-    D3: 'your party',
-    D4: 'their party',
-    D5: 'other party',
-    D6: 'other party',
-    D7: 'other party',
-    D8: 'other party',
-    D9: 'other party',
-    D10: 'other party',
-    D11: 'other party',
-    D12: 'other party',
+    D1: 'CDU',
+    D2: 'SPD',
+    D3: 'Grüne',
+    D4: 'AfD',
+    D5: 'Unabhängige',
+    D6: 'FDP',
+    D7: 'Die Linke',
+    D8: 'Die Basis',
+    D9: 'BIG Sassenburg',
+    D10: 'LKR',
+    D11: 'ÖDP',
+    D12: 'Die Partei',
   }
   const rawdata = 'datum;wahl;ags;gebiet-nr;gebiet-name;max-schnellmeldungen;anz-schnellmeldungen;A1;A2;A3;A;B;B1;C;D;D1_summe_liste_kandidaten;D2_summe_liste_kandidaten;D3_summe_liste_kandidaten;D4_summe_liste_kandidaten;D5_summe_liste_kandidaten\n' +
     '15.10.2017;Landtagswahl;03151000;0;Landkreis Gifhorn;206;206;119699;15654;0;135353;85584;14657;600;2600;700;1000;400;300;200\n'
@@ -76,12 +76,13 @@ const App = () => {
       }
     }
     res.splice(0, 1)
-    setData2(res);
+    setPartyResults(res);
   }
   useEffect(() => {
     Papa.parse(rawdata, {
       header: true,
       complete: function (results) {
+        console.log(results);
         extracted(results.data[0]);
         setLoaded(true)
         setSource('example data')
@@ -103,6 +104,7 @@ const App = () => {
   const fileFromServer = () => {
     Papa.parse(URL + '/Open-Data-Kreiswahl-NDS2191.csv', {
       download: true,
+      header: true,
       complete: function (results) {
         console.log(results);
         extracted(results.data[0]);
@@ -140,7 +142,7 @@ const App = () => {
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  {data2.map((party) => (
+                  {partyResults.map((party) => (
                     <TableRow key={party.name}>
                       <TableCell component="th" scope="row">{party.name}</TableCell>
                       <TableCell align="right">{party.votes}</TableCell>
@@ -156,7 +158,7 @@ const App = () => {
                 dataKey="value"
                 startAngle={210}
                 endAngle={-30}
-                data={data2}
+                data={partyResults}
                 cx={200}
                 cy={200}
                 outerRadius={80}
